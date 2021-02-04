c8ba493ead1eba3a733762405c236eb91c1b25a7
saqib ali <saqibalip321@gmail.com>
2021-02-04 13:41:53 -0500

    f

--- src/components/sectionCard.js
+++ src/components/sectionCard.js
@@ -1,23 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import Icon from "./images/profile.jpg";
 import bg from "./images/body-wave2.png"
+import bg2 from "./images/flow.png"
 import resume from "./files/Resume 2A.docx.pdf"
 function SectionCard() {
+    window.addEventListener('scroll', () => {
+        if (window.scrollY < 50) {
+            document.getElementById("navbar").className = "trans";
+            document.getElementById("imagee").className = "sli top-img";
+            document.getElementById("imagee2").className = "sli bottom-img";
+        }
+        else if (window.scrollY > 50) {
+            document.getElementById("navbar").className = "nav-scrolling";
+            document.getElementById("imagee").className = "top-img";
+            document.getElementById("imagee2").className = "bottom-img";
+        }
+    })
     return (
         <div class="main">
             <img src={Icon} />
             <div id="two" class="main-img">
-                <img class="top-img" src={bg} style={{ "max-width": "100%", "width": "100%", "position": "absolute", "left": 0, "top": 0, "transform": "rotate(0)" }} />
-                <img class="bottom-img" src={bg} style={{ "max-width": "100%", "width": "100%", "position": "absolute", "left": 0, "bottom": 0 }} />
+                <img id="imagee" class="top-img" src={bg} style={{ "max-width": "200%", "width": "200%", "position": "absolute", "top": 0, "transform": "rotate(0)" }} />
+                <img id="imagee2" class="bottom-img" src={bg} style={{ "max-width": "200%", "width": "200%", "position": "absolute", "bottom": 0 }} />
                 <h1>
                     SAQIB ALI
                     <br />
                     <p class="aspire">An Aspring Software Engineer</p>
                 </h1>
                 <p>
-                    An undergraduate student at the university of waterloo completing
-                    bachelor's in computer engineering with a strong passion for
-                    software development. If you are looking to hire, please contact
+                    A student of computer engineering with a strong passion for
+                    software development and solid industry experience.
+                    If you are looking to hire, please contact
                     me here:
                 </p>
                 <div class="section-container-btns">
