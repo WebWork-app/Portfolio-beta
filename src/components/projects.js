a9552a5064c30fe6b5b35f86a5ca3d7a500e1c7f
saqib ali <saqibalip321@gmail.com>
2021-02-09 23:05:13 -0500

    update

--- src/components/projects.js
+++ src/components/projects.js
@@ -82,7 +82,7 @@ function Project() {
                                             <a
                                                 href={(Object.entries(project)[0][1][2] == "") ? ("javascript:void(0)"):("https://github.com/SaqibA1i/"+Object.entries(project)[0][1][2])}
                                                 class={(Object.entries(project)[0][1][2] == "") ? ("disabled main-btn"):("main-btn")}>
-                                                <Github style={{ "padding-right": "10px" }} size={30} />
+                                                <Github style={{ "padding-right": "10px"}} size={30} />
                                                 Github
                                                 <CaretRightFill style={{"padding-left":"10px"}} class="arrow" size={20} />
                                             </a>
